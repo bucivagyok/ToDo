75fe6d2a55ef787f9ba2126304dbb0e28d454168
Hunyadi Csenger <jrr6vs@inf.elte.hu>
2025-08-17 18:24:30 +0200

    Moved source files to folder

---
+++ app/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Task a = new Task(3);
+        System.out.println(a.szoveg);
+    }
+}
---
+++ app/Task.java
@@ -0,0 +1,9 @@
+public class Task {
+    int id;
+    String szoveg;
+
+    public Task(int id) {
+        this.id = id;
+        szoveg = "Hello";
+    }
+}
