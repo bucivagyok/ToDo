e2db5cab111a1194b86804554d44fc4184172c64
Hunyadi Csenger <jrr6vs@inf.elte.hu>
2025-08-19 01:36:34 +0200

    Finished core features, removed id

--- app/Job.java
+++ app/Job.java
@@ -1,22 +1,23 @@
 package app;
 
 public class Job {
-    int id;
     String title;
     String description;
     boolean isCompleted;
 
     public Job(int id, String title, String description) {
-        this.id = id;
         this.title = title;
         this.description = description;
         this.isCompleted = false;
     }
     
     public Job(int id, String title) {
-        this.id = id;
         this.title = title;
         this.description = "";
+        this.isCompleted = false;
+    }
+
+    public void Done() {
         this.isCompleted = true;
     }
 }
--- app/JobList.java
+++ app/JobList.java
@@ -13,21 +13,27 @@ public class JobList {
         jobs.add(job);
     }
 
-    public void ReDescribe(int id, String description) {
-        Job toChange = null;
-        for (Job job : jobs) {
-            if (job.id == id) {
-                toChange = job;
-            }
-        }
-        if (toChange != null) {
-            toChange.description = description;
+    public void ReDescribe(int number, String description) {
+        if (number < jobs.size()) {
+            jobs.get(number).description = description;
         }
     }
 
     public void ListJobs() {
-       for (Job job : jobs) {
-            System.out.println(String.format("%d %s %s %s", job.id, job.title, job.description, job.isCompleted ? "Completed" : "Not completed"));
-        } 
+        for (int i = 0; i < jobs.size(); i++) {
+            System.out.println(String.format("%d %s %s %s", i, jobs.get(i).title, jobs.get(i).description, jobs.get(i).isCompleted ? "Completed" : "Not completed"));
+        }
+    }
+
+    public void JobDone(int number) {
+        if (number < jobs.size()) {
+           jobs.get(number).isCompleted = true;
+        }
+    }
+
+    public void DeleteJob(int number) {
+        if (number < jobs.size() && jobs.get(number).isCompleted) {
+            jobs.remove(number);
+        }
     }
 }
